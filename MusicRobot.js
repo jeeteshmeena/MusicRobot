cdc4303421b9d2a0059a109ea4c096ee8be3b2be
Swarna Kishore <swarnakishoree@gmail.com>
2014-10-09 16:00:55 -0500

    Added tunes to robot

--- MusicRobot.js
+++ MusicRobot.js
@@ -3,65 +3,101 @@ function canvasClicked(canvasNumber)
  theCanvas = "canvas"+canvasNumber;
  c = document.getElementById(theCanvas);
  cxt = c.getContext("2d");
+  if(theCanvas === 'canvas0')
+    {
+    var audio = new Audio("mp3Songs/song4demon-laugh.wav.mp3");
+    audio.play();
+   
+    }
  if(theCanvas === 'canvas1')
     {
-    var audio = new Audio("mp3Songs/1.mp3");
+    var audio = new Audio("mp3Songs/14robot-funky-music.wav.mp3");
     audio.play();
-    setTimeout(function(){audio.pause();}, 280);  
+   
     } 
   if(theCanvas === 'canvas2')
     {
-    var audio = new Audio("mp3Songs/2.mp3");
+    var audio = new Audio("mp3Songs/15robot_welcome.aif.mp3");
     audio.play();
-    setTimeout(function(){audio.pause();}, 380);  
+    
     } 
     if(theCanvas === 'canvas3')
     {
-    var audio = new Audio("mp3Songs/3.mp3");
+    var audio = new Audio("mp3Songs/warning-nuclear-cylon-detected.wav.mp3");
     audio.play();
-    setTimeout(function(){audio.pause();}, 680); 
+    
     } 
     if(theCanvas === 'canvas4')
     {
-    var audio = new Audio("mp3Songs/4.mp3");
+    var audio = new Audio("mp3Songs/robot-dee-jay.wav.mp3");
     audio.play();
-    setTimeout(function(){audio.pause();}, 680);  
+    
     } 
     if(theCanvas === 'canvas5')
     {
-    var audio = new Audio("mp3Songs/5.mp3");
+    var audio = new Audio("mp3Songs/robot_wonder.aif.mp3");
     audio.play();
-    setTimeout(function(){audio.pause();}, 680);  
+   
     } 
     if(theCanvas === 'canvas6')
     {
     	
-    var audio = new Audio("mp3Songs/6.mp3");
+    var audio = new Audio("mp3Songs/song10well-that-makes-it-all-okay-then.wav.mp3");
     audio.play();
-    setTimeout(function(){audio.pause();}, 680);  
+      
     } 
     if(theCanvas === 'canvas7')
     {
-    var audio = new Audio("mp3Songs/7.mp3");
+    var audio = new Audio("mp3Songs/song11i-am-a-machine.wav.mp3");
     audio.play();
-    setTimeout(function(){audio.pause();}, 680);  
+      
     } 
     if(theCanvas === 'canvas8')
     {
-    var audio = new Audio("mp3Songs/8.mp3");
+    var audio = new Audio("mp3Songs/song13its-craziness.wav.mp3");
     audio.play();
-    setTimeout(function(){audio.pause();}, 680);  
+    
     } 
     if(theCanvas === 'canvas9')
     {
-    var audio = new Audio("mp3Songs/9.mp3");
+    var audio = new Audio("mp3Songs/song2Laugh.mp3");
     audio.play();
-    setTimeout(function(){audio.pause();}, 680);  
+   
     } 
     if(theCanvas === 'canvas10')
     {
-    var audio = new Audio("mp3Songs/10.mp3");
+    var audio = new Audio("mp3Songs/song5-i-love-you.mp3.mp3");
     audio.play();
-    setTimeout(function(){audio.pause();}, 680);  
+   
     } 
+     if(theCanvas === 'canvas11')
+    {
+    var audio = new Audio("mp3Songs/song6something-wrong-with-this-program.aif.mp3");
+    audio.play();
+   
+    }
+     if(theCanvas === 'canvas12')
+    {
+    var audio = new Audio("mp3Songs/song7swingy-silly-vox-loop.wav.mp3");
+    audio.play();
+   
+    }
+     if(theCanvas === 'canvas13')
+    {
+    var audio = new Audio("mp3Songs/song8wait-a-minute-now.wav.mp3");
+    audio.play();
+   
+    }
+     if(theCanvas === 'canvas14')
+    {
+    var audio = new Audio("mp3Songs/song9well-allright-everybody.wav.mp3");
+    audio.play();
+   
+    }
+     if(theCanvas === 'canvas15')
+    {
+    var audio = new Audio("mp3Songs/that-sounds-good.wav.mp3");
+    audio.play();
+   
+    }
  }
